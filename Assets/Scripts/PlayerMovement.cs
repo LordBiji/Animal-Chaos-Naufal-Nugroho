ade5834a340617bc118b49e9256cc4ed4188fb0e
LordBiji <alexsanderibo01@gmail.com>
2024-12-18 09:27:54 +0700

    Movement player horizontal

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 250f;
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector3 (movementX * moveSpeed, 0f, 0f);
+    }
+}
